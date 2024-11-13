30382baef2cfd382f138147ce94f04d944bfc1a5
barbanakov <barbanakov@gmail.com>
2024-11-13 11:42:50 +0300

    Создана функция для получения изображения с сайта

--- Cataas.py
+++ Cataas.py
@@ -3,6 +3,19 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
+def load_image(url):
+    try:
+        response = requests.get(url) # получен ответ на запрос по ссылке
+        response.raise_for_status() # нужно для обработки исключений (здесь определяется ошибка)
+        image_data = BytesIO(response.content) # контент (картинка) ответа будет обработан в BytesIO
+        img = Image.open(image_data) # image_data открывается через библиотеку PIL и передаётся в img
+        return ImageTk.PhotoImage(img) # функция возвращает картинку из img
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return None
+
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
